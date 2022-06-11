ebcc60c193248c4f6d1efa4fe5b66c8a5e78df66
Lucas Ferreira <lucascandido.ferreira@hotmail.com>
2022-06-11 17:02:38 -0300

    Publicação Heroko

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+app.use(express.static(__dirname + '/dist/lcf'));
+
+app.get('/*', (req, res) =>{
+  res.sendFile(__dirname + 'dist/lcf/index.html');
+})
+
+app.listen(PORT, () =>{
+  console.log('Servidor iniciado na porta ' + PORT);
+})
