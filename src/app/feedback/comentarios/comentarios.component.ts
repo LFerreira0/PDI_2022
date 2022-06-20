23e4b61de81b1a00b544e7f73036d72341efecd6
Lucas Ferreira <lucascandido.ferreira@hotmail.com>
2022-06-20 18:39:07 -0300

    Ajustes finais

--- src/app/feedback/comentarios/comentarios.component.ts
+++ src/app/feedback/comentarios/comentarios.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ComentariosService } from '../comentarios.service';
+import { ComentariosService } from './comentarios.service';
 
 @Component({
   selector: 'app-comentarios',
